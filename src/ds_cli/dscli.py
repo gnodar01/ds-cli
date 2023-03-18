edec531bf5d9a9bb12e1a789162ad4f77b76cec0
Nodari Gogoberidze <gnodar01@gmail.com>
2023-03-18 13:27:00 -0400

    Style rclick

--- src/ds_cli/dscli.py
+++ src/ds_cli/dscli.py
@@ -2,13 +2,13 @@ from rich import print as rprint
 import rich_click as rclick
 
 from . import __version__, CONTEXT_SETTINGS
-from .styles import DS_GREEN_TRUE
+from .styles import DS_GREEN
 
 
 @rclick.command(context_settings=CONTEXT_SETTINGS)
 def hello():
     """Say Hello to the world"""
-    rprint(f"[{DS_GREEN_TRUE}]Hello[/], [bold magenta]World[/]!", ":vampire:")
+    rprint(f"[{DS_GREEN}]Hello[/], [bold magenta]World[/]!", ":vampire:")
     rclick.secho(f"Hello, [bold magenta]World[/bold magenta]! :vampire:")
 
 @rclick.version_option(__version__, '-v', '--version')
