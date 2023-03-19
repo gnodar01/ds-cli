fd33bdd8aa2e3dd232bdb9a136d7a7d76da0ad1c
Nodari Gogoberidze <gnodar01@gmail.com>
2023-03-18 21:58:57 -0400

    Move cli runner to fixture

--- src/tests/test_dscli.py
+++ src/tests/test_dscli.py
@@ -1,8 +1,12 @@
+import pytest
 import click.testing
 
 from ds_cli import dscli
 
-def test_main_succeeds():
-    runner = click.testing.CliRunner()
+@pytest.fixture(scope="function")
+def runner():
+    return click.testing.CliRunner()
+
+def test_main_succeeds(runner):
     result = runner.invoke(dscli.main())
     assert result.exit_code == 0
