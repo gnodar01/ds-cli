3ca4fd1acdd30e6a4154b424abee9f429f8630a4
Nodari Gogoberidze <gnodar01@gmail.com>
2023-03-19 16:35:14 -0400

    Constrained nox
    add nox deps poetry dev deps and install with teh generated requiremetns
    as constraints in nox

--- noxfile.py
+++ noxfile.py
@@ -9,11 +9,29 @@ nox.options.sessions = "lint", "tests"
 locations = "src", "tests", "noxfile.py"
 
 
+def install_with_constraints(session, *args, **kwargs):
+    """Install packages constrained by poetry.lock."""
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--with",
+            "dev,test",
+            # "--format=requirements.txt",
+            "--format=constraints.txt",
+            "--without-hashes",
+            "--output",
+            requirements.name,
+            external=True,
+        )
+        session.install(f"--constraint={requirements.name}", *args, **kwargs)
+
+
 @nox.session(python=["3.8"])
 def black(session):
     # override with nox -s black -- arg1 arg2 ...
     args = session.posargs or locations
-    session.install("black")
+    install_with_constraints(session, "black")
     session.run("black", *args)
 
 
@@ -21,7 +39,8 @@ def black(session):
 def lint(session):
     # override with nox -s lint -- arg1 arg2 ...
     args = session.posargs or locations
-    session.install(
+    install_with_constraints(
+        session,
         "flake8",
         "flake8-black",
         "flake8-import-order",
@@ -39,13 +58,13 @@ def safety(session):
             "export",
             "--with",
             "dev,test",
-            "--without-hashes",
             "--format=requirements.txt",
+            "--without-hashes",
             "--output",
             requirements.name,
             external=True,
         )
-        session.install("safety")
+        install_with_constraints(session, "safety")
         session.run("safety", "check", "--file", requirements.name, "--full-report")
 
 
@@ -56,5 +75,8 @@ def tests(session):
     # poetry is not part of the environment created by Nox,
     # so we specify external to void warnings about external
     # commands leaking into the isolated test environments
-    session.run("poetry", "install", external=True)
+    session.run("poetry", "install", "--without", "dev", external=True)
+    install_with_constraints(
+        session, "coverage[toml]", "pytest", "pytest-cov", "pytest-mock"
+    )
     session.run("pytest", *args)
