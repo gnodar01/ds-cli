a91aac2ca3d15ce85ee1c1b54afc47be38c636a7
Nodari Gogoberidze <gnodar01@gmail.com>
2023-03-20 19:41:19 -0400

    Switch to automatic version inferene

--- src/ds_reify/__init__.py
+++ src/ds_reify/__init__.py
@@ -1,3 +1,9 @@
-__version__ = "0.1.0"
+from importlib.metadata import PackageNotFoundError, version
+
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
 
 CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
